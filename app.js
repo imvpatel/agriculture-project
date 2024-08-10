f1b4ff7b071e6b1d49baf208c26fc5d285355ced
vraj patel <130795718+imvpatel@users.noreply.github.com>
2024-08-10 17:46:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Get the element by its ID (you can also use querySelector, class name, etc.)
+var element = document.getElementById('dhvani');
+
+// Add an event listener to handle the click event
+element.addEventListener('click', function() {
+    // Define the URL to navigate to
+    var newUrl = 'https://www.google.com';
+
+    // Navigate to the new URL
+    window.location.href = www.google.com;
+});
